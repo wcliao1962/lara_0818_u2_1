b53db440eb819870884b222f2262d418ec55c425
廖文忠 <wcliao1962@gmail.com>
2021-10-17 10:25:59 +0800

    2-3 共同區塊建立與使用

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title') | My Blog</title>
 </head>
 <body>
+    @include('layouts.partials.navigation')
     <div class="container">
         @yield('content')
     </div>
---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,8 @@
+<ul>
+    <li>
+        <a href="#">Item 1</a>
+    </li>
+    <li>
+        <a href="#">Item 2</a>
+    </li>
+</ul>
